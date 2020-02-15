1ee744fa3041f6f93accf15cccb1ac9cf620b011
Daniel Fuentes <dfuen026@fiu.edu>
2020-02-15 13:28:54 -0500

    Revert "Revert "created shopping cart class and added some fields to it""
    This reverts commit 59296ee252d79a25d1b55f999698a46d4e45b0d2.

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Author, Genre, Book, Publisher#ShoppingCart
+from .models import Author, Genre, Book, Publisher, ShoppingCart
 
 admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Publisher)
-#admin.site.register(ShoppingCart)
+admin.site.register(ShoppingCart)
