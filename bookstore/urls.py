9a2a759a3c9c9a3610059390d0b40b879f01670e
Daniel Fuentes <dfuen026@fiu.edu>
2020-02-23 15:50:47 -0500

    Attempting to resolve merge conflicts

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('about/', views.about, name = 'about'),
-    path('books/', views.books, name = 'books'),
-    path('contact/', views.contact, name = 'contact'),
-    path('reviews/', views.reviews, name = 'reviews'),
-    path('wishlist/', views.wishlist, name = 'wishlist'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('books/', views.books, name='books'),
+    path('contact/', views.contact, name='contact'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('wishlist/', views.wishlist, name='wishlist'),
 ]
