b5f082046b506fba71b3d87fb3fcb8d683e1e319
amand <afern619@fiu.edu>
2020-03-16 12:38:17 -0400

    Added the all books by author feature

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ContactForm, Book, Author, Genre
 from django.http import HttpResponse, Http404
 from django.views import generic
