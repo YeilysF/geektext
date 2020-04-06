82b133e2d2a37636b41cc3dcd201f091135861d7
Daniel Fuentes <dfuen026@fiu.edu>
2020-04-05 23:26:08 -0400

    Added Password Reset Button + Form in Profile

--- GeekText/urls.py
+++ GeekText/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('register/', user_view.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    url(r'^password/$', views.change_password, name='change_password'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
