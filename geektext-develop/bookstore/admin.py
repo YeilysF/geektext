5b9af5cd0724e03d778367a1f381172e19af4ef5
Daniel Fuentes <dfuen026@fiu.edu>
2020-04-10 19:29:27 -0400

    Remove duplicate geektext-develop folder

--- geektext-develop/bookstore/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Author, Genre, Book, Publisher, Comment
-
-admin.site.register(Book)
-admin.site.register(Genre)
-admin.site.register(Author)
-admin.site.register(Publisher)
-admin.site.register(Comment)
